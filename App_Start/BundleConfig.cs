a58db8d6896cedfc3cba37d22356323d3c117b8c
marciopuntel <114773750+marciopuntel@users.noreply.github.com>
2022-09-30 17:13:38 -0300

    Add files via upload

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿
+using System.Web.Optimization;
+
+namespace ClimaTempoSimples
+{
+    public class BundleConfig
+    {
+        
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/Select2.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+        
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                      "~/Content/Select2.css"));
+        }
+    }
+}
